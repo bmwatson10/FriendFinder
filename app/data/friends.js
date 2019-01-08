19c0f3e3eb7afa1a03c564d4f583b30d5ebf9afb
Briana Watson <breemarie1010@gmail.com>
2019-01-07 17:57:27 -0700

    Updating questions on survey

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Danielle Bird",
+		photo: "https://www.facebook.com/photo.php?fbid=10216400244532031&set=a.1552935976669&type=3&theater",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Jenelle Martinez",
+		photo: "https://www.facebook.com/photo.php?fbid=10217737460929914&set=a.1590125594695&type=3&theater",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Marrissa Capen",
+		photo: "https://www.facebook.com/photo.php?fbid=10213132731057474&set=a.1369632365431&type=3&theater",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
